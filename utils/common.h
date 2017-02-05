da4c747ed30d1b750bdeae979a338b2c9974b9d9
Steve Sutherland <sbsuth@comcast.net>
2017-02-04 22:35:32 -0800

    Add macors for timers

--- utils/common.h
+++ utils/common.h
@@ -4,4 +4,11 @@
 typedef unsigned char uchar;
 typedef unsigned short ushort;
 
+// Set timer scale for the given timer.
+#define PRESCALE_TIMER( iTimer, iScale ) \
+  TCCR##iTimer##B &= ~7; \
+  TCCR##iTimer##B |= iScale
+
+  
+
 #endif
