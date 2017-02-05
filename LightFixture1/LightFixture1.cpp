265dc046f106c3d10be1976dd89f06a0a14ada1f
Steve Sutherland <sbsuth@comcast.net>
2017-02-04 22:39:06 -0800

    First version with Leds

--- LightFixture1/LightFixture1.cpp
+++ LightFixture1/LightFixture1.cpp
@@ -12,7 +12,7 @@
 #include <RF24Ethernet.h>
 #include <ArduinoJson.h>
 
-
+#include "leds.h"
 
 #include "Command.h"
 
@@ -33,12 +33,13 @@
 #define UP_BUTTON_IN 	 35
 #define DOWN_LED_OUT 	 31 
 #define DOWN_BUTTON_IN   33
-#define SPEED_PWM        9
+#define SPEED_PWM        4
 #define CURRENT_LED_OUT  29
 #define BRIGHT_BUTTON_IN 49
 #define DIM_BUTTON_IN    47
 #define RF24_CE          25
 #define RF24_CSN         23
+
 //#else
 //#error "Not an expected Arduino!"
 //#endif
@@ -50,11 +51,10 @@
 
 
 
-
 // Device status/.
 bool goingUp = false;
 bool goingDown = false;
-AvgThresh<16> liftCurrent(518,502,525);
+AvgThresh<16> liftCurrent(500,495,515);
 
 static Switch upButton( UP_BUTTON_IN );
 static Switch downButton( DOWN_BUTTON_IN );
@@ -68,35 +68,62 @@ RF24EthernetClass RF24Ethernet(rf24Radio,rf24Network,rf24Mesh);
 IPAddress myIP(10, 10, 2, 4);
 EthernetServer rf24EthernetServer(1000);
 
+// Leds
+Leds leds;
+
+// Set the prescale values on the timers:
+// Avoid doing timer 0.
+void setupTimers()
+{
+    // On mega for phase correct mode:
+    //  1 : 31 kHz
+    //  2 : 3.9 kHz
+    //  3 : 490 Hz (default)
+    //  4 : 30 Hz
+    //  5 : less
+    //
+    // For fast mode, ~double these.
+    //
+    // Timers for pins:
+    //  #0 : pins 4, 13
+    //  #1 : pins 11, 12
+    //  #2 : pins 9, 10
+    //  #3:  pins 2, 3, 5
+    //  #4:  pins 6, 7, 8
+    //
+    // I use pins 2->8, so timers 2, 3, 4.
+    //int iScale = 1;
+    int iScale = 4;
+    PRESCALE_TIMER( 2, iScale );
+    PRESCALE_TIMER( 3, iScale );
+    PRESCALE_TIMER( 4, iScale );
+}
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(115200);
   Serial.println(F("Start"));
+
+  setupTimers();
   
   pinMode( UP_LED_OUT, OUTPUT);
   pinMode( DOWN_LED_OUT, OUTPUT);
   pinMode( SPEED_PWM, OUTPUT);
   pinMode( CURRENT_LED_OUT, OUTPUT);
 
-Serial.println("A");
+  leds.init();
+
   // Do these after network.being() to avoid mysterious interference....
   upButton.setup();
-Serial.println("B");
   downButton.setup();
-Serial.println("C");
   
   // Ethernet startup.
   Ethernet.begin(myIP);
-Serial.println("D");
   rf24Mesh.begin(MESH_DEFAULT_CHANNEL, RF24_1MBPS, 5000);
-Serial.println("E");
   IPAddress gwIP(10, 10, 2, 2);
-Serial.println("F");
   Ethernet.set_gateway(gwIP);
-Serial.println("G");
   rf24EthernetServer.begin();
-Serial.println("H");
   
   Serial.println(F("Ready"));
 }
@@ -140,12 +167,14 @@ static void updateFixtureRunning()
   #if 1
   bool old = liftCurrent.isInRange();
   int newCurrent = analogRead(CURRENT_IN);
-  liftCurrent.update( newCurrent );
+  int newAvg = liftCurrent.update( newCurrent );
   fixtureRunning = !liftCurrent.isInRange();
   if ( old != liftCurrent.isInRange() ) {
     #if DEBUG_CHANGES
-    Serial.print(F("Fixture running: "));
-    Serial.println(fixtureRunning);
+    //Serial.print(F("Fixture running: "));
+    //Serial.print(newAvg);
+    //Serial.print(F(": "));
+    //Serial.println(fixtureRunning);
     #endif
   }
   #else
@@ -155,9 +184,9 @@ static void updateFixtureRunning()
 }
 void updateHeight()
 {
-    long upPctPerSec = 4;
-    long downPctPerSec = 5;
-    long waitForStart = 500;
+    long upPctPerSec = 42;
+    long downPctPerSec = 50;
+    long waitForStart = 1000;
     long curTime = millis();
     if (goingUp) {
         long incr = (curTime - lastTimeUp);
@@ -171,7 +200,7 @@ void updateHeight()
                 changeUp(false);
             }
         } else {
-            curHeight += (incr * upPctPerSec);
+            curHeight += (incr * upPctPerSec)/10;
             if (curHeight > (100000))
                 curHeight = 100000;
             lastTimeUp = curTime;
@@ -188,7 +217,7 @@ void updateHeight()
                 changeDown(false);
             }
         } else {
-            curHeight -= (incr * downPctPerSec);
+            curHeight -= (incr * downPctPerSec)/10;
             if (curHeight < 0)
                 curHeight = 0;
             lastTimeDown = curTime;
@@ -260,6 +289,23 @@ void processCommand()
             case CmdRFState:
                 rf24EthernetServer.dumpstate();
                 break;
+            case CmdDim: {
+                int pct;
+                if (cmd->arg(0)->getInt(pct)) {
+                    #if 1
+                    leds.dimAll(pct);
+                    #else
+                    //leds.dimOne(WHITE_LED,pct);
+                    leds.dimOne(VIOLET_LED,pct);
+                    //leds.dimOne(ROYAL_BLUE_LED,pct);
+                    //leds.dimOne(BLUE_LED,pct);
+                    //leds.dimOne(CYAN_LED,pct);
+                    //leds.dimOne(RED_LED ,pct);
+                    //leds.dimOne(AMBER_LED,pct);
+                    #endif
+                }
+                break;
+            }
             default:
                 Serial.println(F("Unrecognized cmd\n"));
                 break;
@@ -285,7 +331,9 @@ static void renewMesh()
 
             Serial.print(F("Connection renewal:"));
             Serial.println( rf24Mesh.checkConnection() );
-        } 
+        }  else {
+            //Serial.print(F("Connection good."));
+        }
     }
 }
 
