7cfb4adaa09dccb069c6c2b26814d43b8f5abe9f
Steve Sutherland <sbsuth@comcast.net>
2017-04-17 21:34:33 -0700

    Initial version

---
+++ ROReservoir/ROReservoir.cpp
@@ -0,0 +1,145 @@
+/*
+ */
+
+#include <Arduino.h>
+#include "Switch.h"
+#include "Avg.h"
+
+#include <SPI.h>
+#include <RF24.h>
+#include <RF24Mesh.h>
+#include <RF24Network.h>
+#include <RF24Ethernet.h>
+#include <ArduinoJson.h>
+
+#include "Command.h"
+
+
+#define RF24_CE          14
+#define RF24_CSN         15
+
+
+// Debug prints.
+#define DEBUG_STARTUP 1
+#define DEBUG_CHANGES 1
+#define DEBUG_CMD 1
+#define DEBUG_CONNECT 1
+
+
+
+// Network objects
+RF24 rf24Radio( RF24_CE, RF24_CSN);
+RF24Network rf24Network(rf24Radio);
+RF24Mesh rf24Mesh(rf24Radio,rf24Network);
+RF24EthernetClass RF24Ethernet(rf24Radio,rf24Network,rf24Mesh);
+IPAddress myIP(10, 10, 2, 6);
+EthernetServer rf24EthernetServer(1000);
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(115200);
+  #if DEBUG_STARTUP
+  Serial.println(F("Start"));
+  #endif
+
+  relays.init();
+  
+  // Ethernet startup.
+  Ethernet.begin(myIP);
+  rf24Mesh.begin(MESH_DEFAULT_CHANNEL, RF24_1MBPS, 5000);
+  IPAddress gwIP(10, 10, 2, 2);
+  Ethernet.set_gateway(gwIP);
+  rf24EthernetServer.begin();
+  
+  #if DEBUG_STARTUP
+  Serial.println(F("Ready"));
+  #endif
+}
+
+//static RF24SerialIO rfs( &rf24Network );
+static ArduinoSerialIO sis;
+static CommandParser serialParser( g_commandDescrs, &sis );
+static EthernetSerialIO rfs( &rf24EthernetServer );
+static CommandParser rf24Parser( g_commandDescrs, &rfs );
+static Command serialCmd;
+static Command rf24Cmd;
+
+static void getStatus( Command* cmd )
+{
+    StaticJsonBuffer<200> jsonBuffer;
+
+    JsonObject& json = jsonBuffer.createObject();
+    cmd->ack( json );
+}
+
+void processCommand()
+{
+
+    bool error = false;
+    Command* cmd = 0;
+    if ( (cmd=serialParser.getCommand( &serialCmd, error )) ) {
+    } else if ((cmd = rf24Parser.getCommand( &rf24Cmd, error )) ) {
+    }
+    if (cmd) {
+        bool needResp = true;
+        #if DEBUG_CMD
+        Serial.print(F("Command: "));
+        cmd->printName();
+        Serial.println("");
+        #endif
+        switch ( cmd->kind() ) {
+            case CmdNone:
+                break;
+
+            case CmdPing:
+                break;
+
+            case CmdStatus:
+                getStatus(cmd);
+                break;
+
+            default:
+                #if DEBUG_CMD
+                Serial.println(F("Unrecognized cmd\n"));
+                #endif
+                break;
+        }
+        if (needResp) {
+            cmd->ack();
+        }
+        cmd->parser()->reset();
+    } else if (error) {
+        #if DEBUG_CMD
+        Serial.println(F("Error in command\n"));
+        #endif
+    }
+}
+
+static uint32_t mesh_timer = 0;
+static void renewMesh()
+{
+    uint32_t now = millis();
+    if ((now - mesh_timer) > 30000) {
+        mesh_timer  = now;
+        if( ! rf24Mesh.checkConnection() ){
+            //refresh the network address
+            rf24Mesh.renewAddress();
+
+            #if DEBUG_CONNECT
+            Serial.print(F("Connection renewal:"));
+            Serial.println( rf24Mesh.checkConnection() );
+            #endif
+        }  else {
+            //Serial.print(F("Connection good."));
+        }
+    }
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  renewMesh();
+
+  processCommand();
+}
+
---
+++ ROReservoir/cmds.h
@@ -0,0 +1,16 @@
+#if DEF_CMD_MACROS
+
+#define MAX_ARGS 2
+#define MAX_CMD_LEN 4 // Not including 0.
+
+#else
+
+X(  CmdPing,        "ping", false, CmdArgNone )
+X(  CmdAllOff,      "aoff", false, CmdArgNone )
+X(  CmdOn,          "on",   false, CmdArgInt )
+X(  CmdOff,         "off",  false, CmdArgInt )
+X(  CmdStatus,      "stat", false, CmdArgNone )
+X(  CmdNone,        "nop",  false, CmdArgNone )
+
+#endif
+
