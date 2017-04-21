f38615509ff5559fb0da665176403fdc0376bb9a
Steve Sutherland <sbsuth@comcast.net>
2017-04-20 20:59:34 -0700

    Initial version

---
+++ utils/DistanceSensor.cpp
@@ -0,0 +1,34 @@
+#include "DistanceSensor.h"
+
+// static data decl
+SingleDistanceSensor*    SingleDistanceSensor::m_sensor = 0;
+
+SingleDistanceSensor::SingleDistanceSensor( int trig, int echo )
+    : NewPing( trig, echo, 40 )
+    , m_nextPing(millis())
+    , m_waiting(false)
+{
+    m_sensor = this; // only one!  Could make a vector to expand
+}
+
+// Called from main loop to update and get newest value.
+unsigned short SingleDistanceSensor::update() {
+    if (!m_waiting) {
+        int curTime = millis();
+        if (curTime > m_nextPing) {
+            ping_timer(getEchoCB);
+            m_waiting = true;
+            m_nextPing = curTime + PING_INTERVAL_MS;
+        }
+    }
+    return m_values.avg();
+}
+
+// Non-static timer callback.
+void SingleDistanceSensor::getEcho() {
+    if (check_timer()) {
+        unsigned short distanceCM = (ping_result / US_ROUNDTRIP_CM);
+        m_values.update(distanceCM);
+        m_waiting = false;
+    }
+}
---
+++ utils/DistanceSensor.h
@@ -0,0 +1,32 @@
+#ifndef DISTANCE_SENSOR_H
+#define DISTANCE_SENSOR_H
+
+#include "Avg.h"
+#include "NewPing.h"
+
+#define PING_INTERVAL_MS 20
+
+
+// Class to collect data from a single ultrasonic distance sensor.
+// Keeps a moving avg of 16 values.  Presumes there is only 1 instance
+// in the program!
+class SingleDistanceSensor : public NewPing
+{
+  public:
+    SingleDistanceSensor( int trig, int echo );
+    unsigned short update();
+
+  protected:
+    static SingleDistanceSensor*    m_sensor;
+    static void getEchoCB() {
+        m_sensor->getEcho();
+    }
+    void getEcho();
+
+    Avg<16> m_values;
+    unsigned m_nextPing;
+    bool m_waiting;
+
+};
+
+#endif
