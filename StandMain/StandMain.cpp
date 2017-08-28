f2405b8c17ca2038e169b9bcc04592a8f604aa7c
Steve Sutherland <sbsuth@comcast.net>
2017-08-27 17:17:11 -0700

    Initial version of pump support

--- StandMain/StandMain.cpp
+++ StandMain/StandMain.cpp
@@ -6,14 +6,12 @@
 #include "Avg.h"
 
 #include <SPI.h>
-#include <RF24.h>
-#include <RF24Mesh.h>
-#include <RF24Network.h>
-#include <RF24Ethernet.h>
+#include "RF24Interface.h"
 #include <ArduinoJson.h>
 
 #include "Command.h"
 #include "DistanceSensor.h"
+#include "ControllablePump.h"
 
 
 #define RF24_CE          12
@@ -24,27 +22,45 @@
 
 
 // Debug prints.
-#define DEBUG_STARTUP 1
-#define DEBUG_CHANGES 1
-#define DEBUG_CMD 1
-#define DEBUG_CONNECT 1
+//#define DEBUG_STARTUP 1
+//#define DEBUG_CHANGES 1
+//#define DEBUG_CMD 1
+//#define DEBUG_CONNECT 1
 
 
 
 // Network objects
-RF24 rf24Radio( RF24_CE, RF24_CSN);
-RF24Network rf24Network(rf24Radio);
-RF24Mesh rf24Mesh(rf24Radio,rf24Network);
-RF24EthernetClass RF24Ethernet(rf24Radio,rf24Network,rf24Mesh);
-IPAddress myIP(10, 10, 2, 7);
+RF24IPInterface rf24( 7, RF24_CE, RF24_CSN );
+RF24EthernetClass   RF24Ethernet( rf24.getRadio(), rf24.getNetwork(), rf24.getMesh() );
 EthernetServer rf24EthernetServer(1000);
+//EthernetClient rf24Client;
 
 // Ultrasonic
-SingleDistanceSensor distanceSensor( TRIG, ECHO );
+SingleDistanceSensor distanceSensor( TRIG, ECHO, 50 );
 
 
 // Float switch
 Switch floatSwitch( FLOAT_SW, LOW );
+
+#define NPUMPS 4
+
+#define EE_ADDR(prev) (prev##_EE_ADDR + prev##_EE_SIZE)
+
+#define EE_SIG              123
+#define SIG_EE_ADDR         0
+#define SIG_EE_SIZE         sizeof(unsigned char)
+#define PUMP_EE_ADDR        EE_ADDR(SIG)
+#define PUMP_EE_ADDR_I(i)   (EE_ADDR(SIG) + (i * PUMP_EE_SIZE))
+#define PUMP_EE_SIZE        (NPUMPS*ControllablePump::ee_size())
+
+
+ControllablePump mainCirc( 4, 2, 20, PUMP_EE_ADDR_I(0), rf24 );
+ControllablePump skimmer(  5, 3, 20, PUMP_EE_ADDR_I(1), rf24 );
+ControllablePump ph1(      6, 0, 0,  PUMP_EE_ADDR_I(2), rf24 );
+ControllablePump ph2(      7, 1, 0,  PUMP_EE_ADDR_I(3), rf24 );
+
+ControllablePump* pumps[NPUMPS] = {&mainCirc,  &skimmer, &ph1, &ph2 };
+
 
 // State vars
 
@@ -57,14 +73,19 @@ void setup() {
     Serial.println(F("Start"));
     #endif
 
+    // Check for current signature to determine if settings should be reset.
+    unsigned char sig = 0;
+    EEPROM.get( SIG_EE_ADDR, sig );
+    bool useSettings = (sig == EE_SIG);
+    EEPROM.put( SIG_EE_ADDR, EE_SIG );
+
     floatSwitch.setup();
   
     // Ethernet startup.
-    Ethernet.begin(myIP);
-    rf24Mesh.begin(MESH_DEFAULT_CHANNEL, RF24_1MBPS, 5000);
-    IPAddress gwIP(10, 10, 2, 2);
-    Ethernet.set_gateway(gwIP);
-    rf24EthernetServer.begin();
+    rf24.init();
+
+    for ( int ipump=0; ipump < NPUMPS; ipump++ )
+        pumps[ipump]->setup( useSettings );
 
     // Initial state.
     extPause = false;
@@ -77,7 +98,7 @@ void setup() {
 //static RF24SerialIO rfs( &rf24Network );
 static ArduinoSerialIO sis;
 static CommandParser serialParser( g_commandDescrs, &sis );
-static EthernetSerialIO rfs( &rf24EthernetServer );
+static EthernetSerialIO rfs( &rf24 );
 static CommandParser rf24Parser( g_commandDescrs, &rfs );
 static Command serialCmd;
 static Command rf24Cmd;
@@ -88,6 +109,33 @@ static void getStatus( Command* cmd )
 
     JsonObject& json = jsonBuffer.createObject();
     cmd->ack( json );
+}
+
+void doSwitch( int sw, bool onOff )
+{
+    char buf[8];
+    if (onOff)
+        strcpy( buf, "on " );
+    else
+        strcpy( buf, "off ");
+    char* pc=&buf[0];
+    while (*pc) pc++;
+    *pc++ = '0' + sw;
+    *pc++ = '\n';
+    *pc++ = 0;
+    rf24.sendToRadioClient( 5, buf, 2 );
+}
+
+static void saveSettings()
+{
+    for ( int ipump=0; ipump < NPUMPS; ipump++ )
+        pumps[ipump]->saveSettings();
+}
+
+static void restoreSettings()
+{
+    for ( int ipump=0; ipump < NPUMPS; ipump++ )
+        pumps[ipump]->restoreSettings();
 }
 
 void processCommand()
@@ -116,6 +164,36 @@ void processCommand()
                 getStatus(cmd);
                 break;
 
+            case CmdSaveSettings:
+                saveSettings();
+                break;
+
+            case CmdRestoreSettings:
+                restoreSettings();
+                break;
+
+            case CmdSw: {
+                int sw = cmd->ID();
+                if ((sw >= 7) || (sw < 0))
+                    break;
+                int arg;
+                cmd->arg(0)->getInt(arg);
+                doSwitch( sw, arg ); 
+                break;
+            }
+            case CmdPumpSpeed: {
+                int ipump = cmd->ID();
+                if ((ipump >= NPUMPS) || (ipump < 0))
+                    break;
+                int pct;
+                cmd->arg(0)->getInt(pct);
+                if (pct < 0)
+                    pct = 0;
+                else if (pct > 100)
+                    pct = 100;
+                pumps[ipump]->setSpeed( pct ); 
+                break;
+            }
             default:
                 #if DEBUG_CMD
                 Serial.println(F("Unrecognized cmd\n"));
@@ -134,41 +212,17 @@ void processCommand()
 }
 
 static uint32_t mesh_timer = 0;
-static void renewMesh()
-{
-    uint32_t now = millis();
-    if ((now - mesh_timer) > 30000) {
-        mesh_timer  = now;
-        if( ! rf24Mesh.checkConnection() ){
-            #if DEBUG_CONNECT
-            Serial.println(F("Lost connection. Begin nenew.."));
-            #endif
 
-            //refresh the network address
-            rf24Mesh.renewAddress();
-
-            #if DEBUG_CONNECT
-            Serial.print(F("Connection renewal:"));
-            Serial.println( rf24Mesh.checkConnection() );
-            #endif
-        }  else {
-            #if DEBUG_CONNECT
-            Serial.print(F("Connection good."));
-            #endif
-        }
-    }
-}
-
-unsigned short lastDist = 0;
 
 // the loop function runs over and over again forever
 void loop() {
-  renewMesh();
-  unsigned short dist = distanceSensor.update();
-  if (dist != lastDist) {
+
+  rf24.update();
+
+  unsigned short dist = 0;
+  if (distanceSensor.update(dist) ) {
     //Serial.print("Dist=");
     //Serial.println(dist);
-    lastDist = dist;
   }
   floatSwitch.update();
   if (floatSwitch.changed()) {
@@ -178,5 +232,8 @@ void loop() {
     #endif
   }
   processCommand();
+
+  for ( int ipump=0; ipump < NPUMPS; ipump++ )
+    pumps[ipump]->update();
 }
 
--- StandMain/cmds.h
+++ StandMain/cmds.h
@@ -5,9 +5,13 @@
 
 #else
 
-X(  CmdPing,        "ping", false,  CmdArgNone )
-X(  CmdStatus,      "stat", false,  CmdArgNone )
-X(  CmdNone,        "nop",  false,  CmdArgNone )
+X(  CmdPing,            "ping", false,  CmdArgNone )
+X(  CmdStatus,          "stat", false,  CmdArgNone )
+X(  CmdSw,              "sw",   true,   CmdArgInt )
+X(  CmdPumpSpeed,       "pspd", true,   CmdArgInt )
+X(  CmdSaveSettings,    "sset", false,  CmdArgNone )
+X(  CmdRestoreSettings, "rset", false,  CmdArgNone )
+X(  CmdNone,            "nop",  false,  CmdArgNone )
 
 #endif
 
