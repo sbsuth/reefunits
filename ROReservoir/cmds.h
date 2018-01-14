52fba6476f08e7c5101b098983502412b753ce66
Steve Sutherland <sbsuth@comcast.net>
2018-01-14 10:42:42 -0800

    Add paus and add ID to pon

--- ROReservoir/cmds.h
+++ ROReservoir/cmds.h
@@ -7,11 +7,12 @@
 
 X(  CmdPing,        "ping", false,  CmdArgNone )
 X(  CmdAllOff,      "aoff", false,  CmdArgNone )
-X(  CmdPumpOn,      "pon",  false,  CmdArgInt )
+X(  CmdPumpOn,      "pon",  true,   CmdArgInt )
 X(  CmdPumpOff,     "poff", false,  CmdArgInt )
 X(  CmdStatus,      "stat", false,  CmdArgNone )
 X(  CmdROOn,        "roon", false,  CmdArgInt )
 X(  CmdFill,        "fill", false,  CmdArgInt )
+X(  CmdExtPause,    "paus", false,  CmdArgInt )
 X(  CmdNone,        "nop",  false,  CmdArgNone )
 
 #endif
