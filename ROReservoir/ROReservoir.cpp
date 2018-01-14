3be81e940149cf86831cc2db72ebb837da6301e6
Steve Sutherland <sbsuth@comcast.net>
2018-01-14 10:42:03 -0800

    Convert to new interfaces

--- ROReservoir/ROReservoir.cpp
+++ ROReservoir/ROReservoir.cpp
@@ -1,19 +1,25 @@
 /*
  */
 
+// Debug prints.
+#define DEBUG_STARTUP 0
+#define DEBUG_CHANGES 0
+#define DEBUG_CMD 0
+#define DEBUG_CONNECT 0
+
+#define RF24_RE_INIT_AFTER_NUM_EMPTY 5
+
 #include <Arduino.h>
 #include "Switch.h"
 #include "Avg.h"
 
 #include <SPI.h>
-#include <RF24.h>
-#include <RF24Mesh.h>
-#include <RF24Network.h>
-#include <RF24Ethernet.h>
+#include "RF24Interface.h"
 #include <ArduinoJson.h>
 
 #include "Command.h"
 #include "DistanceSensor.h"
+#include "DecayingState.h"
 
 
 #define RF24_CE          9
@@ -26,20 +32,12 @@
 #define RO_SOLENOID      7
 
 
-// Debug prints.
-#define DEBUG_STARTUP 0
-#define DEBUG_CHANGES 0
-#define DEBUG_CMD 0
-#define DEBUG_CONNECT 0
-
 
 
 // Network objects
-RF24 rf24Radio( RF24_CE, RF24_CSN);
-RF24Network rf24Network(rf24Radio);
-RF24Mesh rf24Mesh(rf24Radio,rf24Network);
-RF24EthernetClass RF24Ethernet(rf24Radio,rf24Network,rf24Mesh);
-IPAddress myIP(10, 10, 2, 6);
+RF24IPInterface rf24( 6, RF24_CE, RF24_CSN, RF24_PA_LOW );
+DEFINE_RF24IPInterface_STATICS;
+RF24EthernetClass   RF24Ethernet( rf24.getRadio(), rf24.getNetwork(), rf24.getMesh() );
 EthernetServer rf24EthernetServer(1000);
 
 // Ultrasonic
@@ -51,6 +49,17 @@ Switch floatSwitch( FLOAT_SW, LOW );
 
 // State vars
 
+// Externally set pump on.
+DecayingState<bool> bottomPumpOn( false, 10*1000UL );
+DecayingState<bool> topPumpOn( false, 10*1000UL );
+DecayingState<bool>* pumpOn[2] = {&topPumpOn, &bottomPumpOn};
+char pumpPins[2] = {TOP_PUMP, BOTTOM_PUMP};
+
+#define I_TOP 0
+#define I_BOT 1
+
+DecayingState<bool> extPause( false, 5 * 60 * 1000UL );
+
 enum ROMode {
     ROForceOff = 0,
     ROForceOn = 1,
@@ -58,8 +67,6 @@ enum ROMode {
 };
 
 ROMode roMode = ROForceOff;
-bool extPause = false;
-bool targetReady[2] = {false,false};
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -69,10 +76,10 @@ void setup() {
     #endif
 
 
-    digitalWrite( TOP_PUMP, 1 );
-    digitalWrite( BOTTOM_PUMP, 1 );
     pinMode( TOP_PUMP, OUTPUT );
     pinMode( BOTTOM_PUMP, OUTPUT );
+    digitalWrite( TOP_PUMP, 1 );
+    digitalWrite( BOTTOM_PUMP, 1 );
 
     digitalWrite( RO_SOLENOID, 0 );
     pinMode( RO_SOLENOID, OUTPUT );
@@ -80,16 +87,10 @@ void setup() {
     floatSwitch.setup();
   
     // Ethernet startup.
-    Ethernet.begin(myIP);
-    rf24Mesh.begin(MESH_DEFAULT_CHANNEL, RF24_1MBPS, 5000);
-    IPAddress gwIP(10, 10, 2, 2);
-    Ethernet.set_gateway(gwIP);
-    rf24EthernetServer.begin();
+    rf24.init();
 
     // Initial state.
-    roMode = ROForceOff;
-    extPause = false;
-    targetReady[0] = targetReady[1] = false;
+    roMode = ROKeepFull;
 
     #if DEBUG_STARTUP
     Serial.println(F("Ready"));
@@ -99,17 +100,81 @@ void setup() {
 //static RF24SerialIO rfs( &rf24Network );
 static ArduinoSerialIO sis;
 static CommandParser serialParser( g_commandDescrs, &sis );
-static EthernetSerialIO rfs( &rf24EthernetServer );
+static EthernetSerialIO rfs( &rf24 );
 static CommandParser rf24Parser( g_commandDescrs, &rfs );
 static Command serialCmd;
 static Command rf24Cmd;
 
 static void getStatus( Command* cmd )
 {
-    StaticJsonBuffer<200> jsonBuffer;
+    StaticJsonBuffer<148> jsonBuffer;
 
     JsonObject& json = jsonBuffer.createObject();
+    json["res_sw"] = floatSwitch.isOn();
+    json["res_lev"] = distanceSensor.currentCM();
+    json["top_on"] = pumpOn[I_TOP]->getVal() ? pumpOn[I_TOP]->timeAtValueSec() : 0;
+    json["bot_on"] = pumpOn[I_BOT]->getVal() ? pumpOn[I_BOT]->timeAtValueSec() : 0;
+    json["ro_mode"] = (int)roMode;
+    json["pause"] = extPause.getVal() / 1000U;
     cmd->ack( json );
+   /*
+    https://bblanchon.github.io/ArduinoJson/assistant/
+   {
+     "res_sw": true,
+     "res_lev": 123,
+     "top_on": 1234,
+     "bot_on": 1234,
+     "ro_mode": 1,
+     "pause": false
+    }
+    134
+   */
+}
+
+// Process a 'pon' cmd.
+// Arg is number of seconds for decay.
+// Response is json giving success and time since on.
+static void setPumpOn( Command* cmd )
+{
+    bool ok = false;
+    unsigned long t = 0;
+    if ((cmd->ID() < 2) && !extPause.getVal()) {
+        DecayingState<bool>* pump = pumpOn[cmd->ID()];
+        t = pump->setVal( true );
+        int decay = 0;
+        cmd->arg(0)->getInt(decay);
+        #if DEBUG_CHANGES
+        Serial.print("PUMP: Turning pump #");Serial.print(cmd->ID());Serial.print(" on for ");Serial.println(decay);
+        #endif
+        if (decay)
+            pump->setDecay(decay * 1000UL);
+
+        ok = true;
+    }
+
+    StaticJsonBuffer<48> jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["ton"] = t / 1000U;
+    json["ok"] = ok;
+    cmd->ack( json );
+   /*
+    https://bblanchon.github.io/ArduinoJson/assistant/
+   {
+     "ton": 1234,
+     "ok": true,
+    }
+    41
+   */
+}
+
+static void updatePumps() 
+{
+    bool ep = extPause.getVal();
+    for ( int i=0; i < 2; i++ ) {
+        DecayingState<bool>* pump = pumpOn[i];
+        pump->update();
+        digitalWrite( pumpPins[i], (!ep && pump->getVal()) ? 0 : 1 );
+    }
 }
 
 void processCommand()
@@ -139,20 +204,21 @@ void processCommand()
                 break;
 
             case CmdAllOff:
-                digitalWrite( TOP_PUMP, 1 );
-                digitalWrite( BOTTOM_PUMP, 1 );
+                pumpOn[I_TOP]->setVal(false);
+                pumpOn[I_BOT]->setVal(false);
                 break;
 
             case CmdPumpOn: {
-                int p;
-                cmd->arg(0)->getInt(p);
-                digitalWrite( p ? BOTTOM_PUMP : TOP_PUMP, 0 );
+                setPumpOn( cmd );
+                needResp = false;
                 break;
             }
             case CmdPumpOff: {
                 int p;
                 cmd->arg(0)->getInt(p);
-                digitalWrite( p ? BOTTOM_PUMP : TOP_PUMP, 1 );
+                if ((p >= 0) && (p < 2)) {
+                    pumpOn[p]->setVal(false);
+                }
                 break;
             }
             case CmdROOn: {
@@ -168,12 +234,22 @@ void processCommand()
                 roMode = (p ? ROKeepFull : ROForceOff);
                 break;
             }
+            case CmdExtPause: {
+                int t = 0;
+                cmd->arg(0)->getInt(t);
+                extPause.setDecay(t * 1000UL);
+                extPause.setVal(true);
+                break;
+            }
             default:
                 #if DEBUG_CMD
                 Serial.println(F("Unrecognized cmd\n"));
                 #endif
                 break;
         }
+        #if DEBUG_CONNECT
+        if (rf24Parser.stream()->connected()) {Serial.println("Disconnecting after command");}
+        #endif
         if (needResp) {
             cmd->ack();
         }
@@ -183,42 +259,16 @@ void processCommand()
         #if DEBUG_CMD
         Serial.println(F("Error in command\n"));
         #endif
-        cmd->disconnect();
     }
 }
 
-static uint32_t mesh_timer = 0;
-static void renewMesh()
-{
-    uint32_t now = millis();
-    if ((now - mesh_timer) > 30000) {
-        mesh_timer  = now;
-        if( ! rf24Mesh.checkConnection() ){
-            #if DEBUG_CONNECT
-            Serial.println(F("Lost connection. Begin nenew.."));
-            #endif
-
-            //refresh the network address
-            rf24Mesh.renewAddress();
-
-            #if DEBUG_CONNECT
-            Serial.print(F("Connection renewal:"));
-            Serial.println( rf24Mesh.checkConnection() );
-            #endif
-        }  else {
-            #if DEBUG_CONNECT
-            Serial.print(F("Connection good."));
-            #endif
-        }
-    }
-}
 
 // Turn on or off the solenoid based on state.
 void updateROOn()
 {
     int onOff = 0;
 
-    if (extPause) {
+    if (extPause.getVal()) {
         // If in external pause mode, always stop.
         onOff = 0;
     } else {
@@ -239,16 +289,15 @@ void updateROOn()
     digitalWrite( RO_SOLENOID, onOff );
 }
 
-unsigned short lastDist = 0;
-
 // the loop function runs over and over again forever
 void loop() {
-  //renewMesh();
-  unsigned short dist = distanceSensor.update();
-  if (dist != lastDist) {
+
+  rf24.update();
+
+  unsigned short dist = 0;
+  if (distanceSensor.update(dist) ) {
     //Serial.print("Dist=");
     //Serial.println(dist);
-    lastDist = dist;
   }
   floatSwitch.update();
   if (floatSwitch.changed()) {
@@ -260,5 +309,6 @@ void loop() {
   processCommand();
 
   updateROOn();
+  updatePumps();
 }
 
